bae2eddc718a33ddfbbd96a1361c7d92c71b8d4f
Vichu3d <39908309+Vichu3d@users.noreply.github.com>
2019-02-27 09:55:12 +0530

    Basic project with necessary dependancies
    Need to look into the project structure which will be updated soon

---
+++ demo_infoM/src/main/java/com/infomanage/demo_infoM/DemoInfoMApplication.java
@@ -0,0 +1,13 @@
+package com.infomanage.demo_infoM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInfoMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInfoMApplication.class, args);
+	}
+
+}
---
+++ demo_infoM/src/test/java/com/infomanage/demo_infoM/DemoInfoMApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infomanage.demo_infoM;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoInfoMApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
